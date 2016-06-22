4cb187eb25ab0f0f374ce33c701f1b6d9e7b655d
Rori Kurniadi <rori.kurniadi@yahoo.com>
2016-06-22 16:50:14 +0700

    jwt auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,14 @@
 <?php
+$app->post('/auth/login', 'AuthController@postLogin');
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+$app->group(['middleware' => 'auth:api'], function ($app) {
+    $app->get('/', function () use ($app) {
+        return $app->version();
+    });
 
-$app->get('/', function () use ($app) {
-    return $app->version();
+    $app->get('/test', function () {
+        return response()->json([
+            'message' => 'Hello World!',
+        ]);
+    });
 });
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,28 +7,39 @@ use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Model implements
-    AuthenticatableContract,
-    AuthorizableContract
+AuthenticatableContract,
+AuthorizableContract
 {
     use Authenticatable, Authorizable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
     protected $fillable = [
         'name', 'email',
     ];
 
     /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
+    * The attributes excluded from the model's JSON form.
+    *
+    * @var array
+    */
     protected $hidden = [
         'password',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
